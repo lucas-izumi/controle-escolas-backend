27e692ed2e72b41ed4f1bd56908e49c02c12b39f
Lucas Izumi <lucasizumi@gmail.com>
2018-07-12 16:46:02 -0300

    Padronização de erros

--- api/escolas/escolasService.js
+++ api/escolas/escolasService.js
@@ -1,4 +1,5 @@
 const Escolas = require('./escolas');
+const _ = require('lodash');
 
 Escolas.methods(['get', 'post', 'put', 'delete']);
 
@@ -6,6 +7,28 @@ Escolas.methods(['get', 'post', 'put', 'delete']);
 //Validar os dados de entrada
 Escolas.updateOptions({new: true, runValidators: true});
 
+//Padronização de erros
+Escolas.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext);
+
+function sendErrorsOrNext(req, res, next) {
+  const bundle = res.locals.bundle;
+
+  if (bundle.errors) {
+    var errors = parseErrors(bundle.errors);
+    res.status(500).json({errors});
+  } else {
+    next(); //Prosseguir com a requisição se não houver erros
+  }
+}
+
+function parseErrors(nodeRestfulErrors) {
+  const errors = [];
+  //Percorre os objetos de erro e pega o atributo "mensagem"
+  _.forIn(nodeRestfulErrors, error => errors.push(error.message));
+  return errors;
+}
+
+//Contador de registros
 Escolas.route('count', function(req, res, next) {
   Escolas.count(function(error, value) {
     if(error) {
